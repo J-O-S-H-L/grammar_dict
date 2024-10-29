142513f5a1f8576098922ea499b64b1f1e029814
Joshua Leiper <143430492+J-O-S-H-L@users.noreply.github.com>
2024-10-29 17:31:49 +0000

    7 html json (#15)
    * Refactor code for improved readability and organization
    
    * Refactor code for improved readability and organization
    
    * Refactor Entry.py: Remove unused imports and comment out incomplete method
    
    * Refactor .vscode/settings.json: Update schema URLs
    
    * move dictionary generation paradigm to HTML -> JSON
    
    * Refactor execution counts in exploring.ipynb

--- dictionary_construction/const.py
+++ dictionary_construction/const.py
@@ -1,45 +1,45 @@
 from bs4 import BeautifulSoup
 import os
 
-path_to_json = r"grammar_pages"
-pages_list = os.listdir(path_to_json)
+# path_to_json = r"grammar_pages"
+# pages_list = os.listdir(path_to_json)
 
 
-def bunpro_adj_options(page_list: list) -> set:
-    adj_options = set()
-    for page in page_list:
-        soup = BeautifulSoup(
-            open(os.path.join(path_to_json, page), "r", encoding="utf-8"), "html.parser"
-        )
-        title = soup.select("ul h4")
-        labels = soup.select("ul > li > p")
-        for t, l in zip(title, labels):
-            if t.get_text(strip=True) == "Part of Speech":
-                adj_options.add(l.get_text(strip=True))
-    return sorted(adj_options)
+# def bunpro_adj_options(page_list: list) -> set:
+#     adj_options = set()
+#     for page in page_list:
+#         soup = BeautifulSoup(
+#             open(os.path.join(path_to_json, page), "r", encoding="utf-8"), "html.parser"
+#         )
+#         title = soup.select("ul h4")
+#         labels = soup.select("ul > li > p")
+#         for t, l in zip(title, labels):
+#             if t.get_text(strip=True) == "Part of Speech":
+#                 adj_options.add(l.get_text(strip=True))
+#     return sorted(adj_options)
 
 
-bun_opt = bunpro_adj_options(pages_list)
+# bun_opt = bunpro_adj_options(pages_list)
 
-yomi_pos = [
-    "adj-na",
-    "adj-na",
-    "adv",
-    "aux-v",
-    "prt",
-    "exp",
-    "adj-na",
-    "n",
-    "prt",
-    "pn",
-    "v-unspec",
-]
-assert len(bun_opt) == len(
-    yomi_pos
-), "Lengths of bunpro and yomichan POS lists are not equal"
-# fix_pos = {key: value for key, value in zip(bun_opt, yomi_pos)}
+# yomi_pos = [
+#     "adj-na",
+#     "adj-na",
+#     "adv",
+#     "aux-v",
+#     "prt",
+#     "exp",
+#     "adj-na",
+#     "n",
+#     "prt",
+#     "pn",
+#     "v-unspec",
+# ]
+# assert len(bun_opt) == len(
+#     yomi_pos
+# ), "Lengths of bunpro and yomichan POS lists are not equal"
+# # fix_pos = {key: value for key, value in zip(bun_opt, yomi_pos)}
 
-fix_pos = {
+FIX_POS = {
     "Adjective": "adj-na",
     "Adjective + Conjunctions": "adj-na",
     "Adverb": "adv",
